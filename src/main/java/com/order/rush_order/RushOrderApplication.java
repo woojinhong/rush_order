f12ad2bbab4ae6204033c5b69232914d9539e5a3
WooJin Hans Hong <orolzleim@gmail.com>
2024-10-16 15:36:15 +0900

    Init

---
+++ src/main/java/com/order/rush_order/RushOrderApplication.java
@@ -0,0 +1,13 @@
+package com.order.rush_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RushOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/order/rush_order/RushOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.rush_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RushOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
